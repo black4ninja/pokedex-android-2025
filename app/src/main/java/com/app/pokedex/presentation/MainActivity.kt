cb5b04bd31403e9fa16beb011d585df04353d967
Alejandro Fernández <alfervil4@gmail.com>
2025-01-29 20:34:19 -0600

    feat: Funcionalidad laboratorio 3 interfaces con compose

---
+++ app/src/main/java/com/app/pokedex/presentation/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.app.pokedex.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.pokedex.presentation.navigation.PokemonNavGraph
+import com.app.pokedex.presentation.theme.PokedexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PokedexTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    PokemonNavGraph()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PokedexTheme {
+        Greeting("Android")
+    }
+}
